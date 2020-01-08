f01e79847c72be3a0a2b480673569b694089be59
nishantkpdh <nishantkpdh07@gmail.com>
2020-01-08 13:40:55 +0000

    day2

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 //call() apply() bind()
 
+// const num=100
+// function Value(){
+//   console.log(`value is ${num}`)
+//   console.log(`value is ${this.num}`)
+// }
+// function Getsamenumber(){
+//   console.log(`value is same ${this.num}`)
+// }
+// var obj1 = { num : 200 , value: Getsamenumber };
+// obj1.value()
 
-const player1={
-  name:'abc',
-  skill:'xyz',
-  life:2,
-  resetlife:function(newdata){
-    return this.life+=newdata;
-  }
+
+// const obj={
+//   name:"abc",
+//   dance:function(){
+//     console.log(this);
+//     var cook=(function(){
+//       console.log(this)
+//     }.bind(this))();
+//     cook;
+//   }
+// }
+// obj.dance()
+
+Date.prototype.lastyear=function(){
+  return this.getFullYear()-1
 }
+var data=new Date('1990-10-10').lastyear();
+console.log(data);
 
-const player2={
-  name:'def',
-  skill:'stu',
-  life:1
-}
-
-
-console.log(player2)
-player1.resetlife.call(player2,5);
-console.log(player2)
-const newplayer2=player1.resetlife.bind(player2,3);
-newplayer2()
-console.log(player2)
 
 
